c2b1ed5364df2ca13dbfe7a5e65fd4e2eef4b4a7
Jonathan Chang <jonoschang7@gmail.com>
2014-05-29 14:31:32 -0400

    Code for new and create action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,11 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
 	end
 
 	def edit
@@ -22,4 +24,8 @@ class PostsController < ApplicationController
 	def destroy
 	end
 
+  private
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
 end
